4b4f241af45db5c29307cc3a34c53864dfb8fd56
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-03-15 23:18:01 -0400

    prettier: add base config

--- functions/prettier.js
+++ functions/prettier.js
@@ -9,7 +9,10 @@ function prettier() {
   files.forEach(file => {
     fs.readFile(file, 'utf-8', (err, data) => {
       if (err) console.error(err)
-      // TODO: write a separate function to handle parser type by reading the file extension
+      /**
+       * TODO: write a separate function to handle parser type by reading the
+       * TODO: file extension, or may be see if we can use `filePath` option here
+       */
       fs.writeFile(file, _prettier.format(data, {trailingComma: 'all', jsxSingleQuote: true, jsxBracketSameLine: false, parser: file.includes('.html') ? 'html' : 'babel'}), (err) => {
         if (err) console.error(err)
       })
