80da8ee93821baa3c11d4dec93cfe24d50660dab
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-03-24 23:58:59 -0400

    chore: eslint

--- functions/eslint.js
+++ functions/eslint.js
@@ -3,6 +3,7 @@ const _eslint = require('eslint')
 function eslint() {
   //TODO: read the project config file and resolve here to proceed
   //TODO: set the same project config file path for the extension in .vscode/settings.json
+  // TODO: work on linting the files
 }
 
 module.exports = eslint
