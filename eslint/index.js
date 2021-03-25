477e25f1792b96ddcd9c71273cd0bc991f8d5293
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-03-25 17:25:04 -0400

    eslint: add some very base config

--- eslint/index.js
+++ eslint/index.js
@@ -1,2 +1,16 @@
 //TODO: export a base config file from here
 // TODO: project config file can extend this core config
+const baseESLintConfig = {
+  //TODO: document all these options
+  env: {
+    browser: true,
+    es6: true
+  },
+  // what is the order of extends and plugins? does it matter?
+  extends: [],
+  plugins: [],
+  rules: {},
+  parserOptions: {}
+}
+
+module.exports = baseESLintConfig
