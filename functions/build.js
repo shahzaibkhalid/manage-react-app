36611a7ab111821b0a631f99b14ead085fc3539b
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-03-31 02:25:29 -0400

    compiler: centralize webpackConfig logic

--- functions/build.js
+++ functions/build.js
@@ -1,30 +1,14 @@
 const util = require('util') // TODO: remove it
 const compiler = require('../compiler');
-const { ENV, FOLDER_NAMES, PROJECT_CONFIG_FILES, resolvePath } = require('../utils');
+const { ENV, getFinalWebpackConfig } = require('../utils');
 
 function build(env) {
   process.env.NODE_ENV = ENV.prod
   process.env.__MRA_PROJECT_ENV__ = env
   try {
-    const baseProjectSpecificConfig = require(
-      resolvePath(
-        FOLDER_NAMES.config,
-        FOLDER_NAMES.envs,
-        PROJECT_CONFIG_FILES.BASE
-      )
-    );
-    const envProjectSpecificConfig = require(
-      resolvePath(
-        FOLDER_NAMES.config,
-        FOLDER_NAMES.envs,
-        env
-      )
-    );
-    const finalCompilerConfig = compiler.merge(
-      baseProjectSpecificConfig.webpack(),
-      envProjectSpecificConfig.webpack()
-    );
-    compiler.build(finalCompilerConfig)
+    compiler.build(
+      getFinalWebpackConfig(env)
+    )
     // console.log(util.inspect(finalCompilerConfig, {showHidden: false, depth: null}))
   } catch(e) {
     console.log('error ', e)
--- functions/start.js
+++ functions/start.js
@@ -1,31 +1,15 @@
 const util = require('util') // TODO: remove it
 const compiler = require('../compiler');
-const { ENV, FOLDER_NAMES, PROJECT_CONFIG_FILES, resolvePath } = require('../utils');
+const { ENV, getFinalWebpackConfig } = require('../utils');
 
 function start(env) {
   process.env.NODE_ENV = ENV.dev;
   process.env.__MRA_PROJECT_ENV__ = env;
   try {
-    const baseProjectSpecificConfig = require(
-      resolvePath(
-        FOLDER_NAMES.config,
-        FOLDER_NAMES.envs,
-        PROJECT_CONFIG_FILES.BASE
-      )
+    // console.log(util.inspect(getFinalWebpackConfig(env), {showHidden: false, depth: null}))
+    compiler.run(
+      getFinalWebpackConfig(env)
     );
-    const envProjectSpecificConfig = require(
-      resolvePath(
-        FOLDER_NAMES.config,
-        FOLDER_NAMES.envs,
-        env
-      )
-    );
-    const finalCompilerConfig = compiler.merge(
-      baseProjectSpecificConfig.webpack(),
-      envProjectSpecificConfig.webpack()
-    );
-    // console.log(util.inspect(finalCompilerConfig, {showHidden: false, depth: null}))
-    compiler.run(finalCompilerConfig);
   } catch(e) {
     console.log('error while running the dev server ', e);
   }
