d8f62eb3674bd4b99dff5e6e9015c79d8b761c73
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-09 20:59:24 -0400

    test: remove jest-preset file

--- test/jest-preset.js
+++
@@ -1,8 +0,0 @@
-const { resolvePath, FOLDER_NAMES } = require('../utils')
-
-const baseJestConfig = {
-  // write the base, project agnostic config here
-  rootDir: resolvePath(FOLDER_NAMES.src)
-}
-
-module.exports = baseJestConfig
