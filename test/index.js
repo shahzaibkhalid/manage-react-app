f13714813405e986bdc5901d3f0d2b571edd1c7d
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-10 23:53:39 -0400

    test: add docs

--- test/index.js
+++ test/index.js
@@ -2,9 +2,13 @@ const deepmerge = require('deepmerge')
 const { resolvePath, FOLDER_NAMES } = require('../utils')
 
 function getBaseJestConfig() {
-  // write the base config here
   // TODO: document all the options
   return {
+    /**
+     * @rootDir
+     * To point-out the source-code directory that Jest
+     * should be using and looking out for tests
+     */
     rootDir: resolvePath(FOLDER_NAMES.src)
   }
 }
