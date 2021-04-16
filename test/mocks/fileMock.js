5f72a7c554b90ca2f404eacb09b6f8a977d7ce01
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-16 14:46:30 -0400

    test: document Jest mocks for static resources

--- test/mocks/fileMock.js
+++ test/mocks/fileMock.js
@@ -1,1 +1,5 @@
+/**
+ * This mock is applied whenever a bunch of different
+ * static assets are encountered in the source code by Jest
+ */
 module.exports = 'test-file-stub';
--- test/mocks/styleMock.js
+++ test/mocks/styleMock.js
@@ -1,1 +1,5 @@
+/**
+ * This mock is applied when stylesheets (CSS/LESS) are
+ * encountered in the source code by Jest
+ */
 module.exports = {};
