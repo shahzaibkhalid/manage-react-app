a59de59a0613c573871f7cdedb947e9e37ba1d72
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-16 14:42:40 -0400

    webpack: add documentation for font-loader

--- compiler/config/misc/loaders/font-loader.js
+++ compiler/config/misc/loaders/font-loader.js
@@ -1,4 +1,10 @@
-//TODO: document this
+/**
+ * @fontWebpackLoader
+ * This function returns the loader config for a
+ * variety of common font file extensions that can
+ * be resolved by webpack. Starting v5, we need the
+ * built-in `asset/resource` to handle these files
+ */
 function fontWebpackLoader() {
   return {
     test: /\.(woff|woff2|eot|ttf|otf)$/i,
