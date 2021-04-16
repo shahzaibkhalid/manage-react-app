a802ad7a5a0c9e778ce05449d225d0185aabeadf
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-16 14:44:23 -0400

    test: documentation for the Jest transform

--- test/babelTransformForJest.js
+++ test/babelTransformForJest.js
@@ -1,5 +1,13 @@
-//TODO: document why this file exists?
-// what is the use of babel-jest?
+/**
+ * Why this file exists?
+ *
+ * As explained in the `transform` property description of
+ * the Jest config, Jest doesn't know how to parse non-standard
+ * JavaScript, like, JSX. Using an official package named `babel-jest`,
+ * we are passing it the same babel config that we pass to webpack.
+ * In this way, Jest applies these transformation to the test code and
+ * understand non-standards APIs and syntax.
+ */
 const babelJest = require('babel-jest')
 const { getFinalBabelConfig } = require('../utils')
 
