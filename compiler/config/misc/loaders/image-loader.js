5e9bbc1586310d32628ad74a36d8a28fd95ad6e4
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-16 14:42:52 -0400

    webpack: add documentation for image-loader

--- compiler/config/misc/loaders/image-loader.js
+++ compiler/config/misc/loaders/image-loader.js
@@ -1,4 +1,10 @@
-//TODO: document this
+/**
+ * @imageWebpackLoader
+ * This function returns the loader config for a
+ * variety of common image file extensions that can
+ * be resolved by webpack. Starting v5, we need the
+ * built-in `asset/resource` to handle these files
+ */
 function imageWebpackLoader() {
   return {
     test: /\.(png|svg|jpg|jpeg|gif)$/i,
