dc0ef1eba799db2648df7e9c04141aaeb13eaf9a
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-23 18:58:33 -0400

    hooks: add list of ci checks

--- hooks/common.js
+++ hooks/common.js
@@ -5,4 +5,13 @@ const test = require('../functions/test')
 const audit = require('../functions/audit')
 
 //TODO: implement checks here.
-
+/**
+ * CHECKS
+ * 1) check tests
+ * 2) check ESLint
+ * 3) check Lockfile
+ * 4) check formatting of commit message
+ * 5) run performance audit (TBD if this has to be on pre-push?)
+ * 6) check branch name
+ * 7) format source code
+ */
