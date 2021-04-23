808922c52d1b2f5476ef76f628163bfeb08ca625
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-04-22 23:32:54 -0400

    quality: start adding new metrics

---
+++ hooks/common.js
@@ -0,0 +1,8 @@
+// all common checks goes here
+
+const eslint = require('../functions/eslint')
+const test = require('../functions/test')
+const audit = require('../functions/audit')
+
+//TODO: implement checks here.
+
