6976941263117ef50eb63c7725b5db4a09bbbd3e
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-05-09 23:38:46 -0400

    feat(hooks): updates to various hooks

--- hooks/shared.js
+++ hooks/shared.js
@@ -1,6 +1,8 @@
 // all shared checks goes here
 const fs = require('fs')
-const branchNameLint = require('branch-name-lint')
+const BranchNameLint = require('branch-name-lint')
+const readCommitMessage = require('@commitlint/read').default
+const lintCommitMessage = require('@commitlint/lint').default
 const eslint = require('../functions/eslint')
 const test = require('../functions/test')
 const { resolvePath, FILE_NAMES } = require('../utils')
@@ -24,7 +26,10 @@ function eslintCICheck() {
       process.exitCode = (errorCount || warningCount) ? 1 : 0;
       //TODO: explain your intention with this piece of code
       if (process.exitCode === 1) {
+        console.log('Lint checks failed! ❌')
         process.exit(1)
+      } else {
+        console.log('All lint checks are passing! ✅')
       }
     })
     .catch(error => {
@@ -41,28 +46,29 @@ function lockFileCICheck() {
   const yarnLockPath = resolvePath(FILE_NAMES.yarnLock)
   const npmLockPath = resolvePath(FILE_NAMES.npmLock)
 
-  fs.promises.access(yarnLockPath, fs.constants.F_OK)
-    .then(() => {
-      console.log(`${FILE_NAMES.yarnLock} exists! ✅`)
-      fs.promises.access(npmLockPath, fs.constants.F_OK)
-        .then(() => {
-          console.error(`${FILE_NAMES.npmLock} exists! ❌`)
-          process.exit(1)
-        })
-        .catch(() => {
-          console.log(`${FILE_NAMES.npmLock} does not exist! ✅`)
-          process.exitCode = 0;
-        })
-    })
-    .catch(() => {
-      console.error(`${FILE_NAMES.yarnLock} does not exist! ❌`)
-      process.exit(1)
-    })
+  const yarnLockFileExists = fs.existsSync(yarnLockPath)
+  const npmLockFileExists = fs.existsSync(npmLockPath)
+
+  if (npmLockFileExists) {
+    console.error(`${FILE_NAMES.npmLock} exists! ❌`)
+    process.exit(1)
+  }
+
+  if (!yarnLockFileExists) {
+    console.error(`${FILE_NAMES.yarnLock} does not exist! ❌`)
+    process.exit(1)
+  }
+
+  if (yarnLockFileExists && !npmLockFileExists) {
+    console.log(`${FILE_NAMES.yarnLock} exists! ✅`)
+    console.log(`${FILE_NAMES.npmLock} does not exist! ✅`)
+    process.exitCode = 0
+  }
 }
 
 function branchNameCICheck() {
-  branchNameLint({
-    //only a branch with these prefixes are allowed
+  const branchNameLint = new BranchNameLint({
+    // only a branch with these prefixes are allowed
     prefixes: [
       'feature',
       'bugfix',
@@ -74,7 +80,7 @@ function branchNameCICheck() {
       'tests',
       'tools'
     ],
-    //following suggestions can be offered for common error
+    // following suggestions can be offered for common error
     // for example: use `feature` instead of `feature`
     suggestions: {
       features: 'feature',
@@ -82,18 +88,32 @@ function branchNameCICheck() {
       fix: 'bugfix',
       releases: 'release'
     },
-    //pushing to the following branches is not allowed
+    // pushing to the following branches is not allowed
     disallowed: [
       'main',
       'master',
       'develop',
       'staging'
     ],
-  })
+  });
+
+  const processCode = branchNameLint.doValidation();
+  if (processCode === 1) {
+    console.log('Some git branch lint checks failed! ❌');
+    process.exit(1);
+  } else {
+    console.log('All Git branch lint checks passed! ✅');
+    process.exitCode = 0;
+  }
 }
 
 function commitMessageCICheck() {
-  //TODO: implement commit message check here.
+  const commitMessage = fs.readFileSync(process.argv[2], 'utf8').trim()
+  lintCommitMessage(
+    commitMessage,
+    //TODO: use conventional commit base config
+  )
+  // process.exit(1)
 }
 
 
@@ -113,6 +133,6 @@ module.exports = {
  * 3) check Lockfile ✅
  * 4) check formatting of commit message
  * 5) run performance audit (TBD if this has to be on pre-push?)
- * 6) check branch name
+ * 6) check branch name ✅
  * 7) format source code
  */
