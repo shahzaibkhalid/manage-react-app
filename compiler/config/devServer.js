67b6ad04b7fbb365dff042c46b130d31627c1dfe
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-05-16 23:34:56 -0400

    webpack(dev-server): add new config options

--- compiler/config/devServer.js
+++ compiler/config/devServer.js
@@ -14,10 +14,21 @@ module.exports = {
    */
   hot: true,
   /**
+   * @noInfo
+   *
    * Tells dev-server to suppress messages like the webpack bundle
    * information. Errors and warnings will still be shown.
    * We are logging most stuff through custom `run` script
    * TODO: see if we need to use quiet instead of noInfo
    */
   noInfo: true,
+  /**
+   * @historyApiFallback
+   *
+   * Prior to SPA routing, a route change was trigerred full-blow through
+   * server, `historyApiFallback` tells webpack that we are building a SPA
+   * and it should utilize the History API instead of trying to hit the
+   * server when we navigate and change routes.
+   */
+  historyApiFallback: true,
 }
