c786e5aeb56525f6a41fed194d2bf32a3c67caa0
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-12-14 00:58:16 +0500

    feat(babel): :zap: add support for `corejs` to polyfill non-supported feats

--- utils/index.js
+++ utils/index.js
@@ -231,7 +231,13 @@ function getFinalBabelConfig() {
           'importSource': '@theme-ui/core'
         }
       ],
-      require.resolve('@babel/preset-env'),
+      [
+        require.resolve('@babel/preset-env'),
+        {
+          useBuiltIns: 'usage',
+          corejs: "3.0"
+        }
+      ]
     ],
   }
 }
