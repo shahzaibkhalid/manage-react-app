a35329c78db15d42c881f56314b38e1157644a08
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-12-15 21:19:33 +0500

    fix(webpack): :bug: fix broken `run` script on `webpack-dev-server@v5`

--- compiler/functions/run.js
+++ compiler/functions/run.js
@@ -1,21 +1,14 @@
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const { PROJECT_VARS } = require('../../utils');
 
 function run(config) {
   const compiler = Webpack(config);
-  const server = new WebpackDevServer(compiler, config.devServer);
+  const server = new WebpackDevServer(config.devServer, compiler);
 
-  const { PROTOCOL, HOST, DEV_PORT } = PROJECT_VARS;
-
-  server.listen(
-    DEV_PORT,
-    HOST,
-    () => console.log(`
-      Serving source code on port ${DEV_PORT}! 🎉
-      ${PROTOCOL}${HOST}:${DEV_PORT}
-    `)
-  )
+  async function runServer() {
+    await server.start();
+  }
+  runServer();
 }
 
 module.exports = run;
