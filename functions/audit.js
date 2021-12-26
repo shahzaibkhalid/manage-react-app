c3bd295a6dd7eeffbb93e27d29eb4244a6461abe
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-12-26 19:47:31 +0500

    feat(audit): :sparkles: add core `audit` functionality

--- functions/audit.js
+++ functions/audit.js
@@ -1,3 +1,38 @@
-function audit() {}
-  //TODO: implement web audit, like core web vitals and lighthouse
+const fs = require('fs');
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+const serve = require('./serve')
+const { resolvePath, PROJECT_VARS, FOLDER_NAMES, FILE_NAMES } = require('../utils')
+
+async function generateAndOpenAuditReport() {
+  const { PROTOCOL, HOST, SERVER_PORT } = PROJECT_VARS;
+  const finalAuditConfig = require(resolvePath(
+    FOLDER_NAMES.config,
+    FOLDER_NAMES.codeQuality,
+    FILE_NAMES.auditConfig
+  ));
+
+  const url = PROTOCOL + HOST + ':' + SERVER_PORT;
+
+  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+  const runnerResult = await lighthouse(url, {...finalAuditConfig, port: chrome.port});
+
+  const reportHtml = runnerResult.report;
+  const reportPath = resolvePath(FOLDER_NAMES.dist, FOLDER_NAMES.report, FILE_NAMES.auditReport);
+  fs.writeFileSync(
+    reportPath,
+    reportHtml
+  );
+
+  console.log('Report is done for', runnerResult.lhr.finalUrl);
+
+  await chromeLauncher.launch({startingUrl: reportPath});
+
+  await chrome.kill();
+};
+
+function audit() {
+  serve();
+  generateAndOpenAuditReport();
+}
 module.exports = audit
