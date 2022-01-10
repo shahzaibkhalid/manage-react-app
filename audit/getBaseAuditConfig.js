b104adc46837907a54c7db5c3258d0593bc37227
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2022-01-09 20:09:01 -0500

    docs(audit): :memo: add details about base config

--- audit/getBaseAuditConfig.js
+++ audit/getBaseAuditConfig.js
@@ -1,7 +1,15 @@
 function getBaseAuditConfig() {
-  //todo: explain these options
   return {
+    /**
+     * @logLevel
+     * Setting `info` means only information about the performance
+     * should be reported
+     */
     logLevel: 'info',
+    /**
+     * @output
+     * Audit report should be saved in HTML format
+     */
     output: 'html',
   }
 }
